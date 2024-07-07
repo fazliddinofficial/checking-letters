ffde4514aafa38d5e0259aeec10fd530e946ba32
fazliddinofficial <fazliddinquvatboyev8@gmail.com>
2024-07-07 07:17:38 -0700

    problems have fixed

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-var text = prompt("Enter text...");
-var letter = prompt("Enter your letter");
+var text = prompt("Enter text...").toLowerCase();
+var letter = prompt("Enter your letter").toLowerCase();
 
 var arr = text.split(" ");
 var word = arr[2];
-if(letter === word.charAt(0)){
-    console.log("Letter is equal to the first letter)");
+if(letter === word.charAt(0) || letter === word){
+    console.log("Letter or word is equal to the first letter or third word)");
 }
 else{
     console.log("They are not match(");
 }
+console.log(arr);
+console.log(word);
