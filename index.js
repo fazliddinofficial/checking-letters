6d6eb51ef3dd8c3aa07a394e782ebc665338d9e5
fazliddinofficial <fazliddinquvatboyev8@gmail.com>
2024-07-07 20:13:10 -0700

    Better version has been made

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ var letter = prompt("Enter your letter").toLowerCase();
 
 var arr = text.split(" ");
 var word = arr[2];
-if(letter === word.charAt(0) || letter === word){
-    console.log("Letter or word is equal to the first letter or third word)");
+// if(letter === word.charAt(0) || letter === word){
+//     console.log("Letter or word is equal to the first letter or third word)");
+// }
+// else{
+//     console.log("They are not match(");
+// }
+
+if(word.startsWith(letter)){
+    console.log(true);
 }
 else{
-    console.log("They are not match(");
+    console.log(false);
 }
+console.log(word.startsWith(letter));
 console.log(arr);
 console.log(word);
